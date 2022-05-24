e04af8cc75b49323cb44e88fcf705e6848b656e6
JabirMJ2 <jabirmj2@gmail.com>
2022-05-24 11:56:54 +0530

    20

---
+++ trig.sql
@@ -0,0 +1,30 @@
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE TRIGGER display_salary_changes
+BEFORE DELETE OR INSERT OR UPDATE ON customer
+FOR EACH ROW
+WHEN(NEW.ID > 0)
+DECLARE
+	sal_diff number;
+BEGIN
+	sal_diff:= :NEW.salary - :OLD.salary;
+	dbms_output.put_line('Old Salary: ' || :OLD.salary);
+	dbms_output.put_line('New Salary: ' || :NEW.salary);
+	dbms_output.put_line('Salary difference: ' || sal_diff);
+END;
+/
+
+DECLARE
+	total_rows number(2);
+BEGIN
+	UPDATE customer
+	SET salary = salary + 5000;
+
+	IF sql%notfound THEN
+		dbms_output.put_line('no customers updated');
+	ELSIF sql%found THEN
+		total_rows:= sql%rowcount;
+		dbms_output.put_line(total_rows || ' customers updated ');
+	END IF;
+END;
+/	
